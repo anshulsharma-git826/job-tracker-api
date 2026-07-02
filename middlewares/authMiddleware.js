6eb426d82f06b50e02c4c03e84a19fc65c49d0dd
anshulsharma-git826 <anshul3579sharma@gmail.com>
2026-07-02 21:20:34 +0530

    Add complete user auth - register, login, JWT middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const User = require('../models/User');
 
 const protect = async (req, res, next) => {
     try{
@@ -7,13 +7,14 @@ const protect = async (req, res, next) => {
         if ( req.headers.authorization && req.headers.authorization.startsWith('Bearer') ){
           token = req.headers.authorization.split(' ')[1];
         }
+
         if (!token) {
             return res.status(401).json({ success:false, message: 'Not authorized, no token'});
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        req.user = await User.findByID(decoded.id).select('-password');
+        req.user = await User.findById(decoded.id).select('-password');
 
         next();
     } catch (error) {
